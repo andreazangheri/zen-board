7387d87705008ddbf381e9806846474e7733edbd
Andrea Zangheri <arkznh@gmail.com>
2018-10-26 23:07:12 +0200

    src pack

---
+++ src/dockMenu.js
@@ -0,0 +1,15 @@
+const { Menu } = require('electron')
+
+  module.export = new Menu.buildFromTemplate([
+    {
+      label: 'New Window',
+      click () { console.log('New Window') }
+    }, {
+      label: 'New Window with Settings',
+      submenu: [
+        { label: 'Basic' },
+        { label: 'Pro' }
+      ]
+    },
+    { label: 'New Command...' }
+  ])
