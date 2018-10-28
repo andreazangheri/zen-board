445119463af873ff714391d686139a1f9b4c4c17
Andrea Zangheri <arkznh@gmail.com>
2018-10-28 16:25:14 +0100

    main/config.js

---
+++ main/config.js
@@ -0,0 +1,26 @@
+var appConfig = require("application-config")("ZenBoard");
+var fs = require("fs");
+var path = require("path");
+
+var APP_NAME = "ZenBoard";
+var APP_TEAM = "typerror";
+var APP_VERSION = require("../package.json").version;
+
+module.exports = {
+  APP_COPYRIGHT: "Copyright © 2018" + APP_TEAM,
+  APP_ICON: path.join(__dirname, "../assets", "icon"),
+  APP_NAME: APP_NAME,
+  APP_TEAM: APP_TEAM,
+  APP_VERSION: APP_VERSION,
+
+  GITHUB_URL: "https://github.com/typerror/zen-board",
+  GITHUB_URL_ISSUES: "https://github.com/typerror/zen-board/issues",
+  GITHUB_URL_RAW: "https://raw.githubusercontent.com/typerror/zen-board/master",
+
+  HOME_PAGE_URL: "https://google.com",
+
+  ROOT_PATH: __dirname,
+  ASSETS_PATH: path.join(__dirname, "../assets"),
+  ICON_PATH: path.join(__dirname, "../assets/icon.icns"),
+  STYLE_PATH: path.join(__dirname, "./style/style.css")
+};
